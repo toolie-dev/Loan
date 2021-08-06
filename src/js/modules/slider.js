72dfafcda70402564785b2461d4da72b21fb2e4f
masterjourney <masterjourney1@gmail.comy>
2021-08-06 12:15:02 +0300

    Loan version 1.1.0: Added main slider and popup

---
+++ src/js/modules/slider.js
@@ -0,0 +1,49 @@
+import Popup from "./popup";
+const popup = new Popup('.modules__info .hanson', 3000, '.modules');
+export default class Slider {
+    constructor(page, btns) {
+        this.page = document.querySelector(page);
+        this.slides = this.page.children;
+        this.btns = document.querySelectorAll(btns);
+        this.slideIndex = 1;
+    }
+
+    showSlides(n) {
+        if (n > this.slides.length) {
+            this.slideIndex = 1;
+        }
+
+        if (n < 1) {
+            this.slideIndex = this.slides.length;
+        }
+
+        this.slides.forEach(slide => {
+            slide.style.display = 'none';
+            slide.classList.remove('animated', 'fadeInUp');
+        });
+
+        this.slides[this.slideIndex - 1].style.display = 'block';
+        this.slides[this.slideIndex - 1].classList.add('animated', 'fadeInUp');
+        popup.showPopup();
+    }
+
+    plusSlides(n) {
+        this.showSlides(this.slideIndex += n);
+    }
+
+    render() {
+        this.btns.forEach(item => {
+            item.addEventListener('click', () => {
+                this.plusSlides(1);
+            });
+
+            item.parentNode.previousElementSibling.addEventListener('click', (e) => {
+                e.preventDefault();
+                this.slideIndex = 1;
+                this.showSlides(this.slideIndex);
+            });
+        });
+
+        this.showSlides(this.slideIndex);
+    }
+}
